bcc5d98cbe8af4486a449d5767d9ea7a6b333380
Stameskaaa <chta02@mail.ru>
2024-06-19 18:20:13 +0500

    123

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ require('dotenv').config();
 console.log(process.env.MONGO_URL);
 app.use(bodyParser.json());
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.setHeader('Access-Control-Allow-Origin', process.env.SRC);
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
   next();
